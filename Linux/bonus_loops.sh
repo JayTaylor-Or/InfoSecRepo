c56a396ec9f0cd09f546bafdf6d9a37be23e501b
Jay Taylor <88463451+JayTaylor-Or@users.noreply.github.com>
2022-02-13 12:19:16 -0800

    Diagram to GitHub

--- Linux/bonus_loops.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-nums=$(echo {0..9})
-for num in ${nums[@]};
-do
-  new_num=$(($num + 2))
-  echo $new_num
-
-
-done
--- Linux/super_bonus_loops.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-executables=$(find /home -type f -perm 777 2> /dev/null)
-
-for files in ${executables[@]}
-do
-  echo $files
-done
